76fb08ea7ed60989a49d7e4bb6788d487e998d0e
Jeffry Hesse <jhesse@sonatype.com>
2020-04-08 16:45:18 -0800

    Initial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      persons: [],
+      person: {}
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:8080/persons/list')
+      .then((res) => res.json())
+      .then((data) => {
+        this.setState({ persons: data})
+      })
+      .catch(console.log)
+  }
+
+  render() {
+    return (
+      <React.Fragment>
+          {this.state.persons.map((x) => (
+            <h1>{x.Person}</h1>
+          ))}
+      </React.Fragment>
+    );
+  }
 }
 
 export default App;
